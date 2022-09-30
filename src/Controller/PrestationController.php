dbca81c7c320b846f042c1d3235c5d7448953fa8
DNOLLE27 <damien.nolle@stadjutor.com>
2022-09-30 17:34:03 +0200

    <feature>(Préstations) : Début page de prestations + données bdd

--- src/Controller/PrestationController.php
+++ src/Controller/PrestationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Prestations;
 
 class PrestationController extends AbstractController
 {
@@ -13,8 +14,13 @@ class PrestationController extends AbstractController
      */
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Prestations::class);
+
+        $prestations = $repo->findAll();
+
         return $this->render('prestation/index.html.twig', [
             'controller_name' => 'PrestationController',
+            'Prestations' => $prestations
         ]);
     }
 }
