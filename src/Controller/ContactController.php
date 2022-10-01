c2347c355026cb5d238fb7b2ccaa6268139fe297
DNOLLE27 <damien.nolle@stadjutor.com>
2022-10-01 17:35:16 +0200

    <feature>(Contact) : Début page contact (form + style)

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ContactType;
+use App\Entity\Contact;
 
 class ContactController extends AbstractController
 {
@@ -13,8 +15,12 @@ class ContactController extends AbstractController
      */
     public function index(): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class,$contact);
+
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
+            'ContactType' => $form->createView()
         ]);
     }
 }
