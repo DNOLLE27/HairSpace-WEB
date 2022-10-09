82cadb255fdd2937a378475ff6c8e9c2060a135f
Romain Zabeth <romain.zabeth@gmail.com>
2022-10-09 12:35:34 +0200

    <feature>(presentation): presentation fini  + ajustement

--- src/Controller/CommentaireController.php
+++ src/Controller/CommentaireController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use App\Entity\Avis;
 use App\Entity\Utilisateurs;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -23,7 +24,7 @@ class CommentaireController extends AbstractController
             
             
             $form = $this->createFormBuilder($avis)
-                         ->add('avs_commentaire')
+                         ->add('avs_commentaire', TextareaType::class, ['attr' => ['rows' => 7,'cols' => 30]])
                          ->getForm();
                          
             $form->handleRequest($request);
